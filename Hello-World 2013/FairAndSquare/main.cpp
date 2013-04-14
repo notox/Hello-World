bab6b9129d1cb079b1a5d740af2aa868ad11ef89
Jay Xia <notoxxia@gmail.com>
2013-04-14 21:05:16 +0800

    sprintf的速度好慢。

--- Hello-World 2013/FairAndSquare/main.cpp
+++ Hello-World 2013/FairAndSquare/main.cpp
@@ -4,54 +4,20 @@
 
 #define FOUND 1
 
-int getValue(double m, int i)
+int isFair(const char *a)
 {
-	double t = m / pow(10.0, i);
-	modf(t, &t);
-
-	int v = (int)t;
-	v %= 10;
-	
-	return v;
-}
-
-int isFair(double m)
-{
-	int i,j;
-	double k;
-	int n = 1;
-	k = m;
-
-	while (k != 0)
-	{
-		k /= 10;
-		modf(k, &k);
-
-		if (k != 0)
-			n++;
-	}
-
-	for(i = 0; i<n; i++)
-	{
-		if (i == n/2)
-			return FOUND;
-
-		if (getValue(m, i) != getValue(m, n-(i+1)))
+	int len = strlen(a);
+	for (int i = 0; i < len/2; ++i)
+		if (*(a+i) != *(a+len-i-1))
 			return !FOUND;
-	}
 	return FOUND;
-}
-
-double nextFair(double d)
-{
-	// Not implemented
-	return 0.0;
 }
 
 int main()
 {
 	int i, n, r;
 	double j, k, s, e;
+	char h[14];
 	bool sqrtFound;
 
 	freopen("a.in","r",stdin);
@@ -84,7 +50,11 @@ int main()
 					sqrtFound = true;
 			}
 
-			if (isFair(j) && isFair(k))
+			sprintf(h, "%.0lf", k);
+			if (!isFair(h))
+				continue;
+			sprintf(h, "%.0lf", j);
+			if (isFair(h))
 				r++;
 		}
 
