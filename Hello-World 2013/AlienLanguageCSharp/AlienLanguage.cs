81722a8ed7af3a08278c0093acff2cbb8b01d9b2
夏勇杰 <notoxxia@gmail.com>
2013-04-25 14:02:57 +0800

    尝试Visual Studio下写单元测试。

---
+++ Hello-World 2013/AlienLanguageCSharp/AlienLanguage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlienLanguageCSharp
+{
+	public class AlienLanguage
+	{
+		private List<string> dic = new List<string>();
+
+		public int process(string sentence)
+		{
+			return 1;
+		}
+
+		public void add(string word)
+		{
+			dic.Add(word);
+		}
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ Hello-World 2013/AlienLanguageCSharpUT/AlienLanguageUT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlienLanguageCSharp;
+
+namespace AlienLanguageCSharpUT
+{
+	[TestClass]
+	public class AlienLanguageUT
+	{
+		[TestMethod]
+		public void say_abc()
+		{
+			AlienLanguage language = new AlienLanguage();
+			int actual = language.process("abc");
+			Assert.AreEqual(1, actual);
+		}
+		
+		//[TestMethod]
+		//public void say_abc_with_brasket()
+		//{
+		//    AlienLanguage language = new AlienLanguage();
+		//    int actual = language.process("(ab)bc");
+		//    Assert.AreEqual(2, actual);
+		//}
+	}
+}
