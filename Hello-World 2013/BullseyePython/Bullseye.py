b74b87cc7f2b60784a57d1e24e7e0ec321b4b994
Yongjie <notoxxia@gmail.com>
2013-05-10 07:51:20 +0800

    使用max函数简化代码

--- Hello-World 2013/BullseyePython/Bullseye.py
+++ Hello-World 2013/BullseyePython/Bullseye.py
@@ -2,9 +2,7 @@ re = input()
 for ri in xrange(re):
   r, t = map(int, raw_input().split())
   used = 0
-  j = int(((t/2)**0.5 + 1 - 2*r)/2)
-  if j < 1:
-    j = 1  
+  j = max(int(((t/2)**0.5 + 1 - 2*r)/2), 1)
   while True:
     used = (2*j + 2*r - 1) * j	
     if used > t:
