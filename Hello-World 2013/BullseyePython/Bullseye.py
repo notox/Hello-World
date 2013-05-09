cdd6962a2b8d48393044c2a06a2b198536b5ac2d
Yongjie <notoxxia@gmail.com>
2013-05-10 07:49:50 +0800

    优化计算方法：直接预测比较接近的值

--- Hello-World 2013/BullseyePython/Bullseye.py
+++ Hello-World 2013/BullseyePython/Bullseye.py
@@ -2,11 +2,13 @@ re = input()
 for ri in xrange(re):
   r, t = map(int, raw_input().split())
   used = 0
-  j = 0
+  j = int(((t/2)**0.5 + 1 - 2*r)/2)
+  if j < 1:
+    j = 1  
   while True:
-    used += 2*r + 4*j + 1	
+    used = (2*j + 2*r - 1) * j	
     if used > t:
       break
     j = j + 1
 	
-  print "Case #" + str(ri + 1) + ": " + str(j)
+  print "Case #" + str(ri + 1) + ": " + str(j-1)
