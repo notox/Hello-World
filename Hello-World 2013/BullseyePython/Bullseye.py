985ab51c320101eeb17e26eae05fd085a67a07f2
Yongjie <notoxxia@gmail.com>
2013-05-10 08:06:42 +0800

    使用二分查找，通过大数据测试。

--- Hello-World 2013/BullseyePython/Bullseye.py
+++ Hello-World 2013/BullseyePython/Bullseye.py
@@ -1,12 +1,19 @@
 re = input()
 for ri in xrange(re):
-  r, t = map(int, raw_input().split())
-  used = 0
-  j = max(int(((t/2)**0.5 + 1 - 2*r)/2), 1)
-  while True:
-    used = (2*j + 2*r - 1) * j	
-    if used > t:
-      break
-    j = j + 1
-	
-  print "Case #" + str(ri + 1) + ": " + str(j-1)
+    r, t = map(int, raw_input().split())
+    begin = max(int((t**0.5 + 1 - 2*r)/2), 1)
+    end = max(int((t/2)**0.5), 1)
+    j = 1
+    while begin <= end:
+        i = (begin + end)/2
+        used = (2*i + 2*r - 1) * i	
+        if used < t:
+            begin = i + 1
+            j = i
+        elif used > t:
+            end = i - 1
+        else:
+            j = i
+            break
+            
+    print "Case #" + str(ri + 1) + ": " + str(j)
