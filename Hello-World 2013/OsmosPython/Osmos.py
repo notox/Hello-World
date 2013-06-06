176f41d6b962d56f0fb4afc61742ac4ab12f5bac
Jay Xia <notoxxia@gmail.com>
2013-06-06 08:06:59 +0800

    2013 RoundB Osmos

---
+++ Hello-World 2013/OsmosPython/Osmos.py
@@ -0,0 +1,29 @@
+re = input()
+for ri in xrange(re):
+  a, n = map(int, raw_input().split())
+  s = map(int, raw_input().split())
+  s.sort()
+  
+  rest = n
+  operateTimes = 0
+  for si in s:
+    if a > si:
+      a += si;
+    elif a == 1:
+      operateTimes += rest
+      break
+    else:    
+      ta = a
+      addTimes = 0
+      while ta <= si:
+        ta = 2 * ta - 1
+        addTimes = addTimes + 1
+      if addTimes > rest:
+        operateTimes += rest
+        break
+      else:
+        operateTimes += addTimes
+        a = ta + si
+    rest -= 1
+          
+  print "Case #" + str(ri + 1) + ": " + str(operateTimes)
