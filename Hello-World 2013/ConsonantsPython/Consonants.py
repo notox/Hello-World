e298883c329d98c03b98fc76b9d2b73f09dfd7d2
Yongjie <notoxxia@gmail.com>
2013-06-11 21:14:07 +0800

    将Consonant改为非递归方式。

--- Hello-World 2013/ConsonantsPython/Consonants.py
+++ Hello-World 2013/ConsonantsPython/Consonants.py
@@ -1,31 +1,36 @@
-def calcConsonants(nvalue, consonants, nameLen, n):
-    if n == 0:
-        return nvalue
-    
+def calcNvalue(preview, next):
+    nvalue = 0
+    if next > 0 and preview > 0:
+        nvalue = (preview+1)*(next+1)
+    elif next == 0 and preview == 0:
+        nvalue = 1
+    elif next == 0:
+        nvalue = (preview + 1)       
+    elif preview == 0:
+        nvalue = (next + 1) 
+       
+    return nvalue
+
+def calcConsonants(consonants, nameLen):
+    nvalue = 0
     consonantsLen = len(consonants)
-    for i in range(0, consonantsLen-n+1):
-        preview = 0        
-        if i == 0:
-            preview = consonants[i]
-        else:
-            preview = consonants[i] - consonants[i-1] - 1
-        
-        next = 0
-        if i == consonantsLen-n:
-            next = nameLen - 1 - consonants[i+n-1] - intn + 1
-        else:
-            next = consonants[i+n] - consonants[i+n-1] - 1   
-        
-        if next > 0 and preview > 0:
-            nvalue += (preview+1)*(next+1)
-        elif next == 0 and preview == 0:
-            nvalue += 1
-        elif next == 0:
-            nvalue += (preview + 1)       
-        elif preview == 0:
-            nvalue += (next + 1)  
+    for j in range(1, consonantsLen+1):
+        for i in range(0, consonantsLen-j+1):
+            preview = 0        
+            if i == 0:
+                preview = consonants[i]
+            else:
+                preview = consonants[i] - consonants[i-1] - 1
             
-    return calcConsonants(nvalue, consonants, nameLen, n - 1)
+            next = 0
+            if i == consonantsLen-j:
+                next = nameLen - 1 - consonants[i+j-1] - intn + 1
+            else:
+                next = consonants[i+j] - consonants[i+j-1] - 1  
+           
+            nvalue += calcNvalue(preview, next)
+            
+    return nvalue
 
 re = input()
 for ri in xrange(re):
@@ -35,12 +40,12 @@ for ri in xrange(re):
     nameLen = len(name)
     consonants = []   
     vowels = ['a', 'o', 'i', 'e', 'u']
-
+    
     for i in range(0, nameLen - intn + 1):
         substring = name[i:intn+i]
         if reduce(lambda x,y: x and y, map(lambda x: substring.find(x) < 0, vowels)):
             consonants.append(i)
-        
-    nvalue = calcConsonants(nvalue, consonants, nameLen, len(consonants))
+    
+    nvalue = calcConsonants(consonants, nameLen)
 
     print "Case #" + str(ri + 1) + ": " + str(nvalue)
