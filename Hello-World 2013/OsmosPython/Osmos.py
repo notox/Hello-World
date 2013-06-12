03106cad79567429514fd4845a35d6bded97ea51
Yongjie <notoxxia@gmail.com>
2013-06-13 06:33:28 +0800

    修改Osmos，修复因为贪婪造成的问题。

--- Hello-World 2013/OsmosPython/Osmos.py
+++ Hello-World 2013/OsmosPython/Osmos.py
@@ -3,7 +3,8 @@ for ri in xrange(re):
   a, n = map(int, raw_input().split())
   s = map(int, raw_input().split())
   s.sort()
-  
+  results = []
+ 
   rest = n
   operateTimes = 0
   for si in s:
@@ -12,12 +13,13 @@ for ri in xrange(re):
     elif a == 1:
       operateTimes += rest
       break
-    else:    
+    else:   
       ta = a
       addTimes = 0
       while ta <= si:
         ta = 2 * ta - 1
         addTimes = addTimes + 1
+      results.append(operateTimes + rest)
       if addTimes > rest:
         operateTimes += rest
         break
@@ -25,5 +27,6 @@ for ri in xrange(re):
         operateTimes += addTimes
         a = ta + si
     rest -= 1
-          
-  print "Case #" + str(ri + 1) + ": " + str(operateTimes)
+  results.append(operateTimes)
+  results.sort()
+  print "Case #" + str(ri + 1) + ": " + str(results[0])
