cc4483abde4b3a2366ea767ece4113c60bd62708
Yongjie <notoxxia@gmail.com>
2013-06-12 15:34:34 +0800

    1.优化代码逻辑，使逻辑更加简单。 2.使下标查找速度更快。

--- Hello-World 2013/ConsonantsPython/Consonants.py
+++ Hello-World 2013/ConsonantsPython/Consonants.py
@@ -1,37 +1,14 @@
-def calcNvalue(preview, next):
-    nvalue = 0
-    if next > 0 and preview > 0:
-        nvalue = (preview+1)*(next+1)
-    elif next == 0 and preview == 0:
-        nvalue = 1
-    elif next == 0:
-        nvalue = (preview + 1)       
-    elif preview == 0:
-        nvalue = (next + 1) 
-       
-    return nvalue
-
 def calcConsonants(consonants, nameLen):
     nvalue = 0
     consonantsLen = len(consonants)
     for j in range(1, consonantsLen+1):
-        for i in range(0, consonantsLen-j+1):
-            preview = 0        
-            if i == 0:
-                preview = consonants[i]
-            else:
-                preview = consonants[i] - consonants[i-1] - 1
-            
-            next = 0
-            if i == consonantsLen-j:
-                next = nameLen - 1 - consonants[i+j-1] - intn + 1
-            else:
-                next = consonants[i+j] - consonants[i+j-1] - 1  
-           
-            nvalue += calcNvalue(preview, next)
+        for i in range(1, consonantsLen-j):
+            preview = consonants[i] - consonants[i-1]
+            next = consonants[i+j] - consonants[i+j-1]            
+            nvalue += preview*next
             
     return nvalue
-
+ 
 re = input()
 for ri in xrange(re):
     name, n = raw_input().split()
@@ -39,13 +16,22 @@ for ri in xrange(re):
     nvalue = 0
     nameLen = len(name)
     consonants = []   
-    vowels = ['a', 'o', 'i', 'e', 'u']
+    vowels = "aioue"
     
-    for i in range(0, nameLen - intn + 1):
-        substring = name[i:intn+i]
-        if reduce(lambda x,y: x and y, map(lambda x: substring.find(x) < 0, vowels)):
-            consonants.append(i)
+    i = 0
+    containConsonant = 0
+    consonants.append(-1)
+    while i < nameLen:
+        if len(set(name[i]).intersection(set(vowels))) == 0:
+            if containConsonant < intn: 
+                containConsonant += 1            
+            if containConsonant == intn:
+                consonants.append(i-intn+1)            
+        else:
+            containConsonant = 0
+        i += 1
+    consonants.append(nameLen-intn+1)   
     
     nvalue = calcConsonants(consonants, nameLen)
-
+    
     print "Case #" + str(ri + 1) + ": " + str(nvalue)
