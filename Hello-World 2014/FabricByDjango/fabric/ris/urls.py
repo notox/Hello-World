19ea64f3a5cbcd518260d72d6745e6fb31294e0c
夏勇杰 <notoxxia@gmail.com>
2014-01-28 16:28:23 +0800

    修改登记功能。

--- Hello-World 2014/FabricByDjango/fabric/ris/urls.py
+++ Hello-World 2014/FabricByDjango/fabric/ris/urls.py
@@ -4,5 +4,5 @@ from ris import views
 
 urlpatterns = patterns('',
 	url(r'^register/$', views.RegisterFormView.as_view(), name='register'),
-	url(r'^register/(?P<study_id>\d+)/$', views.detail, name='register_detail'),
+	url(r'^register/(?P<study_id>\d+)/$', views.RegisterFormUpdateView.as_view(), name='update_register'),
 )
--- Hello-World 2014/FabricByDjango/fabric/ris/views.py
+++ Hello-World 2014/FabricByDjango/fabric/ris/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic.base import View
+from django.forms.widgets import HiddenInput
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
@@ -13,6 +14,7 @@ from django.utils.decorators import method_decorator
 from ris.models import Study, Department, VisitType, Patient
 
 class RegisterForm(forms.Form):
+	pk = forms.IntegerField(required=False, widget=HiddenInput)
 	name = forms.CharField(label=_('Name'), max_length=32)
 	birthday = forms.DateTimeField(label=_('Birthday'))
 	accnum = forms.CharField(label=_('Accnum'),max_length=20)
@@ -22,19 +24,19 @@ class RegisterForm(forms.Form):
 	
 class RegisterFormView(View):
 	form_class = RegisterForm
-	template_name = 'ris/register.html'	
+	template_name = 'ris/register.html'		
 	
 	@method_decorator(login_required)
 	def dispatch(self, *args, **kwargs):
 		return super(RegisterFormView, self).dispatch(*args, **kwargs)
-	
+		
 	def get(self, request, *args, **kwargs):
 		form = self.form_class()
 		latest_study_list = Study.objects.order_by('-pk')[:5]
 		return render(request, self.template_name, {
 			'form': form, 'latest_study_list': latest_study_list
 		})
-		
+	
 	def post(self, request, *args, **kwargs):
 		form = self.form_class(request.POST)
 		latest_study_list = Study.objects.order_by('-pk')[:5]
@@ -47,6 +49,56 @@ class RegisterFormView(View):
 	
 			study = Study(accnum=form.cleaned_data['accnum'], study_date=form.cleaned_data['study_date'], patient=patient, visitType=visittype, department=department)
 			study.save()
+			return HttpResponseRedirect(reverse('ris:register'))
+		
+		return render(request, self.template_name, {
+			'form': form, 'latest_study_list': latest_study_list
+		})
+
+class RegisterFormUpdateView(View):
+	form_class = RegisterForm
+	template_name = 'ris/update_register.html'		
+	
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super(RegisterFormUpdateView, self).dispatch(*args, **kwargs)
+		
+	def get(self, request, *args, **kwargs):
+		study = Study.objects.get(pk=kwargs['study_id'])
+		form = self.form_class({
+			'pk':study.pk,
+			'name':study.patient.name,
+			'birthday':study.patient.birthday,
+			'accnum':study.accnum,
+			'study_date':study.study_date,
+			'visittype':study.visitType.pk,
+			'department':study.department.pk
+		})
+		latest_study_list = Study.objects.order_by('-pk')[:5]
+		return render(request, self.template_name, {
+			'form': form, 'latest_study_list': latest_study_list
+		})
+	
+	def post(self, request, *args, **kwargs):
+		form = self.form_class(request.POST)
+		latest_study_list = Study.objects.order_by('-pk')[:5]
+		if form.is_valid():
+			department = form.cleaned_data['department']
+			visittype = form.cleaned_data['visittype']
+	
+			study_id = form.cleaned_data['pk']
+			study = Study.objects.get(pk=study_id)			
+			patient = study.patient
+			patient.name=form.cleaned_data['name']
+			patient.birthday=form.cleaned_data['birthday']
+			patient.save()
+			
+			study.accnum=form.cleaned_data['accnum']
+			study.study_date=form.cleaned_data['study_date']			
+			study.visitType=visittype
+			study.department=department
+			study.save()
+			
 			return HttpResponseRedirect(reverse('ris:register'))
 		
 		return render(request, self.template_name, {
