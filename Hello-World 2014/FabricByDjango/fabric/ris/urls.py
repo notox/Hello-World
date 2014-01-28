9ccdcd9d8df82dcfa4629b09fb627aae22a2306e
夏勇杰 <notoxxia@gmail.com>
2014-01-28 13:30:17 +0800

    使用Class-based的方法构造登记页面。

--- Hello-World 2014/FabricByDjango/fabric/ris/urls.py
+++ Hello-World 2014/FabricByDjango/fabric/ris/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from ris import views
 
 urlpatterns = patterns('',
-	url(r'^study/register_form/$', views.register_form, name='study_register_form'),
+	url(r'^study/register_form/$', views.RegisterFormView.as_view(), name='study_register_form'),
 	url(r'^study/(?P<study_id>\d+)/$', views.detail, name='study_detail'),
 )
--- Hello-World 2014/FabricByDjango/fabric/ris/views.py
+++ Hello-World 2014/FabricByDjango/fabric/ris/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404, render, render_to_response
+from django.shortcuts import render
 from django.views import generic
+from django.views.generic.base import View
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from django.forms import DateInput
 
 from ris.models import Study, Department, VisitType, Patient
 
@@ -16,12 +16,21 @@ class RegisterForm(forms.Form):
 	study_date = forms.DateTimeField(label=_('Study Date'))
 	department = forms.ModelChoiceField(Department.objects.order_by('-id'), label=_('Department'))
 	visittype = forms.ModelChoiceField(VisitType.objects.order_by('-id'), label=_('VisitType'))
-
-def register_form(request):
-	latest_study_list = Study.objects.order_by('-pk')[:5]
 	
-	if request.method == 'POST':
-		form = RegisterForm(request.POST)
+class RegisterFormView(View):
+	form_class = RegisterForm
+	template_name = 'ris/study/index_form.html'	
+	
+	def get(self, request, *args, **kwargs):
+		form = self.form_class()
+		latest_study_list = Study.objects.order_by('-pk')[:5]
+		return render(request, self.template_name, {
+			'form': form, 'latest_study_list': latest_study_list
+		})
+		
+	def post(self, request, *args, **kwargs):
+		form = self.form_class(request.POST)
+		latest_study_list = Study.objects.order_by('-pk')[:5]
 		if form.is_valid():
 			department = form.cleaned_data['department']
 			visittype = form.cleaned_data['visittype']
@@ -32,13 +41,11 @@ def register_form(request):
 			study = Study(accnum=form.cleaned_data['accnum'], study_date=form.cleaned_data['study_date'], patient=patient, visitType=visittype, department=department)
 			study.save()
 			return HttpResponseRedirect(reverse('ris:study_register_form'))
-	else:
-		form = RegisterForm()
 		
-	return render(request, 'ris/study/index_form.html', {
-		'form': form, 'latest_study_list': latest_study_list
-	})
-	
+		return render(request, self.template_name, {
+			'form': form, 'latest_study_list': latest_study_list
+		})
+
 def detail(request, study_id):
 	return HttpResponse("study id %s" % study_id)	
 	
