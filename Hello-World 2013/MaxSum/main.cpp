80ff301e61de63911e04a89fee3104be61a0c4bd
夏勇杰 <notoxxia@gmail.com>
2014-01-28 09:08:59 +0800

    字数组和O(n)解法。

--- Hello-World 2013/MaxSum/main.cpp
+++ Hello-World 2013/MaxSum/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int MaxSum(int* A, int n)
 {
-	int maximum = A[0];
+	int maximum = A[0]; // if A is empty, it will be a bug.
 	int sum=0;
 	for(int i = 0; i < n; i++)
 	{
@@ -41,6 +41,22 @@ int MaxSum2(int* A, int n)
 	return maximum;
 }
 
+int MaxSum3(int* A, int n)
+{
+	int maximum = A[0];
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (sum >= 0)
+			sum += A[i];
+		else
+			sum = A[i];
+		if (sum > maximum)
+			maximum = sum;
+	}
+	return maximum;
+}
+
 int main() {
 	int arr[] = {1, -2, 3, 10, -4, 7, 2, -5};
 
@@ -48,6 +64,10 @@ int main() {
 	cout << sum << endl;
 
 	sum = MaxSum2(arr, sizeof(arr)/sizeof(arr[0]));
+	cout << sum << endl;
+
+	int arr1[] = {-4, -3, -2, -1};
+	sum = MaxSum3(arr1, sizeof(arr1)/sizeof(arr1[0]));
 	cout << sum << endl;
 
 	return 0;
