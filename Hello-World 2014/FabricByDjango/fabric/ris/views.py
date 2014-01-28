6848c930cff29befb8251e1fa79fdac857d5b1f7
夏勇杰 <notoxxia@gmail.com>
2014-01-28 11:07:28 +0800

    清理没有用的逻辑。

--- Hello-World 2014/FabricByDjango/fabric/ris/views.py
+++ Hello-World 2014/FabricByDjango/fabric/ris/views.py
@@ -17,25 +17,6 @@ class RegisterForm(forms.Form):
 	department = forms.ModelChoiceField(Department.objects.order_by('-id'), label=_('Department'))
 	visittype = forms.ModelChoiceField(VisitType.objects.order_by('-id'), label=_('VisitType'))
 
-def index(request):
-	latest_study_list = Study.objects.order_by('-pk')[:5]
-	
-	departments = Department.objects.order_by('-id')
-	visittypes = VisitType.objects.order_by('-id')
-	
-	context = {'latest_study_list': latest_study_list, 'departments': departments, 'visittypes':visittypes}
-	return render(request, 'ris/study/index.html', context)
-	
-def register(request):
-	department = get_object_or_404(Department, pk=request.POST['department'])
-	visittype = get_object_or_404(VisitType, pk=request.POST['visittype'])
-	patient = Patient(name=request.POST['name'], birthday=request.POST['birthday'])
-	patient.save()
-	
-	study = Study(accnum=request.POST['accnum'], study_date=request.POST['study_date'], patient=patient, visitType=visittype, department=department)
-	study.save()
-	return HttpResponseRedirect(reverse('ris:study_index'))
-	
 def register_form(request):
 	latest_study_list = Study.objects.order_by('-pk')[:5]
 	
@@ -58,25 +39,6 @@ def register_form(request):
 		'form': form, 'latest_study_list': latest_study_list
 	})
 	
-def register_form_update(request):
-	latest_study_list = Study.objects.order_by('-pk')[:5]
-	
-	'''study = get_object_or_404(Study, pk=study_id)
-	form = RegisterForm({'name':study.patient.name, 'birthday':study.patient.birthday, 'accnum':study.accnum, 'study_date':study.study_date, 'department':study.department, 'visittype':study.visitType})'''
-	form = RegisterForm()	
-	
-	return render(request, 'ris/study/index_form_update.html', {
-		'form': form, 'latest_study_list': latest_study_list
-	})
-	
 def detail(request, study_id):
-	return HttpResponse("study id %s" % study_id)
-	
-class IndexView(generic.ListView):
-	context_object_name = 'latest_study_list'
-	template_name = 'ris/study/study_list.html'
-	
-	def get_queryset(self):
-		return Study.objects.order_by('-pk')[:5]
-		
+	return HttpResponse("study id %s" % study_id)	
 	
