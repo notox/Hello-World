85e194185441358f8b6f04c5e593a320f4de53eb
夏勇杰 <notoxxia@gmail.com>
2014-07-02 17:49:15 +0800

    C#Lambda表达式实验。

---
+++ Hello-World language/CSharp/Lambda/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Lambda
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ForEach();
+			Predicate();
+		}
+
+		public static void ForEach()
+		{
+			List<int> nums = new List<int> { 1, 2, 3, 4, 5, 6 };
+			Parallel.ForEach(nums, (value) =>
+			{
+				Console.WriteLine(value);
+			});
+			Console.ReadKey();
+		}
+
+		public static void Predicate()
+		{
+			List<int> nums = new List<int> { 1, 2, 3, 4, 5, 6 };
+			
+			int total = SumAll(nums, n => true);
+			Console.WriteLine(total);
+			total = SumAll(nums, n => n % 2 == 0);
+			Console.WriteLine(total);
+			total = SumAll(nums, n => n > 3);
+			Console.WriteLine(total);
+			Console.ReadKey();
+		}
+
+		public static int SumAll(IEnumerable<int> nums, Predicate<int> predicate)
+		{
+			return nums.Where(i => predicate(i)).Sum();
+		}
+	}
+}
