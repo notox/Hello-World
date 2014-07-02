8608c9ba3350ddbd7ae1384868fce5ba3e27a89a
夏勇杰 <notoxxia@gmail.com>
2014-07-02 14:58:19 +0800

    用Java6写的还没有类型推断。

--- Hello-World language/Java/src/HelloList.java
+++ Hello-World language/Java/src/HelloList.java
@@ -1,6 +1,9 @@
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class HelloList {
     public static void main(String[] args) {
@@ -15,6 +18,58 @@ public class HelloList {
 
         // List<String> namesByNewArrayList = newArrayList("Jay");
         // List<String> namesByOf = of("Jay");
+        Person boy13 = new Person("Jay", 13);
+        Person youngMan = new Person("Adam", 27);
+        Person oldMan = new Person("Andy", 80);
+        List<Person> persons = new ArrayList<Person>();
+        persons.add(boy13);
+        persons.add(youngMan);
+        persons.add(oldMan);
+
+        transformListByOldStyle(persons);
+        transformListByFunctionStyle(persons);
+
+    }
+
+    public static void transformListByOldStyle(List<Person> persons) {
+        List<String> names = new ArrayList<String>();
+        for(Person person : persons) {
+            int age = person.getAge();
+            if (age >= 14 && age <= 28) {
+                names.add(person.getName());
+            }
+        }
+
+        for(String name : names) {
+            System.out.println("Young Man: " + name);
+        }
+    }
+
+    public static void transformListByFunctionStyle(List<Person> persons) {
+        FluentIterable<String> names = FluentIterable.from(persons)
+                                            .filter(isAgeQualified()).transform(toNames());
+        for(String name : names) {
+            System.out.println("Young Man: " + name);
+        }
+    }
+
+    private static Predicate<Person> isAgeQualified() {
+        return new Predicate<Person>() {
+            @Override
+            public boolean apply(Person person) {
+                int age = person.getAge();
+                return age >= 14 && age <= 28;
+            }
+        };
+    }
+
+    private static Function<Person, String> toNames() {
+        return new Function<Person, String>() {
+            @Override
+            public String apply(Person person) {
+                return person.getName();
+            }
+        };
     }
 
 }
---
+++ Hello-World language/Java/src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name = null;
+    private int age = 0;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
