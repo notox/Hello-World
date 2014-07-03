21b8e1c212129325caf56b669a831cc660ed2750
夏勇杰 <notoxxia@gmail.com>
2014-07-03 14:26:10 +0800

    理解闭包上下文变量作用域。

---
+++ Hello-World language/CSharp/Lab/Lambda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab
+{
+	[TestClass]
+	public class Lambda
+	{
+		[TestMethod]
+		public void Closure()
+		{
+			int max = 0;
+			Func<string, bool> predicate = 
+				item =>
+				{
+					max++;
+					return item.Length < max;
+				};
+			var list = new List<string>
+			{
+				"111",
+				"222",
+				"333",
+				"444",
+				"555",
+				"666"
+			};
+			var newList = list.Where(predicate);
+			Assert.AreEqual(3, newList.Count());
+			Assert.IsTrue(newList.Contains("444"));
+			Assert.IsTrue(newList.Contains("555"));
+			Assert.IsTrue(newList.Contains("666"));
+		}
+	}
+}
