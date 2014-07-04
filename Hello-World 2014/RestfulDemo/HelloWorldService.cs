a994f096baeb4d7ecce441b422148ee4b7525823
夏勇杰 <notoxxia@gmail.com>
2014-07-04 13:03:18 +0800

    WCF Restful Demo

---
+++ Hello-World 2014/RestfulDemo/HelloWorldService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestfulDemo
+{
+	[DataContract]
+	public class Person
+	{
+		[DataMember]
+		public string Name { get; set; }
+		[DataMember]
+		public int Age { get; set; }
+		[DataMember]
+		public DateTime Birthday { get; set; }
+	}
+
+	public class HelloWorldService : IService
+	{
+		public string GetHelloWorld()
+		{
+			return "Hello World";
+		}
+
+		public string SayHelloWorld()
+		{
+			return "Hello World";
+		}
+
+		public Person GetPerson()
+		{
+			return new Person { Name = "Jay", Age = 18, Birthday = DateTime.Now };
+		}
+	}
+}
---
+++ Hello-World 2014/RestfulDemo/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestfulDemo
+{
+	[ServiceContract]
+	public interface IService
+	{
+		[OperationContract]
+		[WebGet(UriTemplate="HelloWorld", ResponseFormat = WebMessageFormat.Json)]
+		string GetHelloWorld();
+
+		[OperationContract]
+		[WebGet(UriTemplate = "Persons", ResponseFormat = WebMessageFormat.Json)]
+		Person GetPerson();
+
+		[OperationContract(Name="SayHelloWorld")]
+		string SayHelloWorld();
+	}
+}
