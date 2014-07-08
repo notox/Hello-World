728053fe210455e70b586ea2adb9ccfe0b062f5c
夏勇杰 <notoxxia@gmail.com>
2014-07-08 18:55:21 +0800

    修改引用和修改内容的区别。

---
+++ Hello-World language/CSharp/Lab/Reference.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab
+{
+	[TestClass]
+	public class Reference
+	{
+		public void ModifyReference(Customer customer)
+		{
+			customer = new Customer{ Name = "张三" };
+		}
+
+		public void ModifyContent(Customer customer)
+		{
+			customer.Name = "张三";
+		}
+
+
+		[TestMethod]
+		public void Execute()
+		{
+			var customer = new Customer{ Name = "李四" };
+			ModifyReference(customer);
+			Assert.AreEqual("李四", customer.Name);
+			ModifyContent(customer);
+			Assert.AreEqual("张三", customer.Name);
+		}
+	}
+}
